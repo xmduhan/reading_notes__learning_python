eb0de81da8dec85a4cdb37ff21fbf6d799475f79
xmduhan <xmduhan@gmail.com>
2014-02-24 16:30:39 +0800

    ...

---
+++ ch26-1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 19 17:52:52 2014
+
+@author: Administrator
+"""
+#%%
+class rec : pass
+#%%
+rec.name = 'name'
+#%%
+a = rec()   
+a.name
+
+
+
+#%%
+
+
