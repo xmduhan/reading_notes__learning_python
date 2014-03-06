a1a0b278af8531d66d35cfe5ddb0f7debd0d1764
xmduhan <xmduhan@gmail.com>
2014-03-06 10:02:18 +0800

    ...

---
+++ ex01-VBA/ex01-02-PPT新建保存打开.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+
+#%% 导入相关库文件
+import win32com.client, sys
+
+#%% 启动PowerPoint
+application = win32com.client.Dispatch("PowerPoint.Application")
+application.Visible = True
+
+#%% 新增一个PPT文件,相当于ppt中的按了"新建"菜单
+presentation = application.Presentations.Add()
+# Slides.Add(PageIndex=页面插入位置,LayoutIndex=版式编号)
+presentation.Slides.Add(1,1)
+presentation.Slides.Add(2,2)
+presentation.Slides.Add(3,3)
+
+#%% 保存到文件
+filename = r'c:\1.ppt'
+presentation.SaveAs(filename)
+presentation.Close()
+
+#%% 从一个路径打开文件
+presentation = application.Presentations.Open(filename)
+
+
+
